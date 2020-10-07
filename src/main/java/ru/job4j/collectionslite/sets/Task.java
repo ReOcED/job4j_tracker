d18da9296e40f24d3421291e02bc49c5f7ea7468
ReOcED <nxnbatgo@yandex.ru>
2020-10-07 15:27:15 +0300

    1.3.2.2

---
+++ src/main/java/ru/job4j/collectionslite/sets/Task.java
@@ -0,0 +1,31 @@
+package ru.job4j.collectionslite.sets;
+
+import java.util.Objects;
+
+public class Task {
+    private String number;
+    private String description;
+
+    public Task(String number, String description) {
+        this.number = number;
+        this.description = description;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return number.equals(task.number);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
